42f737f6b995fbf8bc8a690f7c79b2ad50d1bbd8
Michael George <michael-talbert>
2019-09-09 09:19:20 -0700

    Math practice

---
+++ Geometry.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Geometry here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Geometry
+{
+    public static void main (String[] args){
+        int sides = 7;
+        int radius = 5;
+        final double PI = 22/7;
+        System.out.println("A heptagon has "+ sides+" sides");
+        System.out.println("A decagon has "+ (sides+3)+" sides");
+        System.out.println("A dodecagon has "+ (2*sides+6)+" sides");
+        System.out.println("The circumfrence of a circle with a radius of "+radius+" is "+(2*radius*PI));
+}
+}
