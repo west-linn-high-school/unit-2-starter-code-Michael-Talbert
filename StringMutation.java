acc9e7b3fe84fe2613521ed8e698979025873b5f
Michael Talbert <michael-talbert>
2019-09-12 09:43:09 -0700

    String Mutation changes

--- StringMutation.java
+++ StringMutation.java
@@ -16,5 +16,14 @@ public class StringMutation
         System.out.println("phrase length is "+phrasreLength+"and the phrase is "+phrase);
        String mut1, mut2, mut3, mut4;
 
+       mut1 = phrase +" except from vending machines";
+       System.out.println(mut1);
+       System.out.println("length of mut1 = "+ mut1.length());
+
+       mut2 = mut1.toUpperCase();
+       System.out.println(mut2);
+
+       mut3 = mut2.replace("E","X");
+       System.out.println(mut3);
     }
 }
